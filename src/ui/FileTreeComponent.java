651c9fbb8de47e67d47e404394661012ea78124d
UBC Student <rchan05@students.cs.ubc.ca>
2024-02-16 10:03:51 -0800

    UI set paths implementation (#24)
    * added setPaths implementations
    refactored variable names in Program with more accurate variable names
    
    * Apply suggestions from code review
    
    Co-authored-by: UBC Student <mazenk@students.cs.ubc.ca>
    
    * removed commented code and unused import
    
    ---------
    
    Co-authored-by: Ronald Chan <ronaldc.rcws@gmail.com>
    Co-authored-by: UBC Student <mazenk@students.cs.ubc.ca>

--- src/ui/FileTreeComponent.java
+++ src/ui/FileTreeComponent.java
@@ -1,8 +1,12 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Collection;
 
 /**
  * Represents a file tree to display, using a list of files
@@ -26,7 +30,42 @@ public class FileTreeComponent extends JTree {
      *
      * @param paths All the paths to display, including parents.
      */
-    public void setPaths(List<Path> paths) {
-        // TODO actually render paths on this component
+    public void setPaths(String targetDirectory, Collection<Path> paths) {
+        // convert absolute paths to relative paths for displaying in the tree neatly
+        List<Path> relativePaths = paths.stream().map((path ->
+                Paths.get(path.toString().substring(targetDirectory.length())))
+        ).toList();
+
+        DefaultTreeModel model = (DefaultTreeModel) getModel();
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+        root.setUserObject(targetDirectory);
+        // clear previous children
+        root.removeAllChildren();
+        for (Path part : relativePaths) {
+            DefaultMutableTreeNode parent = root;
+            // create nodes for each directory until it reaches end of path
+            for (Path sub : part) {
+                DefaultMutableTreeNode child = getChildNode(parent, sub.toString());
+                // if child does not exist create the node and continue
+                if (child == null) {
+                    child = new DefaultMutableTreeNode(sub.toString());
+                    parent.add(child);
+                }
+                parent = child;
+            }
+        }
+        model.reload();
+    }
+
+    // returns the child node if it exists, if not return null
+    private DefaultMutableTreeNode getChildNode(DefaultMutableTreeNode node, String childName) {
+        int count = node.getChildCount();
+        for (int i = 0; i < count; i++) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) node.getChildAt(i);
+            if (child.getUserObject().toString().equals(childName)) {
+                return child;
+            }
+        }
+        return null;
     }
 }
